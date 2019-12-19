2ee98519d2ec71c0c15afa6639e6d5139bf13af0
takeru1205 <takeru0639418@gmail.com>
2019-12-19 10:48:28 +0900

    [feature] Boids Model Implemented

--- main.py
+++ main.py
@@ -1,38 +1,163 @@
-from tkinter import *
 import random
-from fish import Bird
-
-root = Tk()
-
-WIDTH = 1000
-HEIGHT = 1000
-
-fish_radian = 10
-bird_num = 30
-SPEED = 5
-VIEW = 50
-
-c = Canvas(root, width=WIDTH, height=HEIGHT)
-c.pack()
+from statistics import mean
+from tkinter import *
+import math
 
 
-fish_list = [Bird(random.randint(0, WIDTH), random.randint(0, HEIGHT), random.randint(-SPEED, SPEED),
-                  random.randint(-SPEED, SPEED), i, (WIDTH, HEIGHT, fish_radian, SPEED), [], VIEW) for i in range(bird_num)]
-[fish.set_others(fish_list) for fish in fish_list]
+class Field:
+    WIDTH = 1000
+    HEIGHT = 1000
 
 
-def animate():
-    c.delete("all")
-    for fish in fish_list:
-        fish.draw()
-        c.create_oval(fish.x - fish_radian, fish.y - fish_radian, fish.x+fish_radian, fish.y+fish_radian)
-        c.create_line(fish.x, fish.y, fish.x + fish.vx * 3, fish.y + fish.vy * 3)
-    [fish.set_others(fish_list) for fish in fish_list]
-    root.after(20, animate)
+class Coordinate():
 
-animate()
-root.mainloop()
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.x = 0
+        self.y = 0
 
 
+class Bird():
+    NUM = 30
+    RADIAN = 10
+    SPEED = 4
+    VIEW = 100
+
+    @staticmethod
+    def setup():
+        Bird.birds = [Bird() for _ in range(Bird.NUM)]
+
+    def __init__(self):
+        self.x = random.randint(0, Field.WIDTH)
+        self.y = random.randint(0, Field.HEIGHT)
+        self.vx = random.randint(-self.SPEED, self.SPEED)
+        self.vy = random.randint(-self.SPEED, self.SPEED)
+        self.view = self.VIEW
+        self.r1 = 1.0  # cohesion coefficient
+        self.r2 = 0.8  # separation coefficient
+        self.r3 = 0.3  # alignment coefficient
+
+        self.neighbors = None
+
+        self.v1 = Coordinate()
+        self.v2 = Coordinate()
+        self.v3 = Coordinate()
+
+    def find_neighbors(self):
+        self.neighbors = Bird.birds
+
+    # 結合
+    def cohesion(self):
+        if not self.neighbors:
+            return
+        center_pull_coeff = 300
+
+        self.v1.x = mean([agent.x for agent in self.neighbors if agent is not self])
+        self.v1.y = mean([agent.y for agent in self.neighbors if agent is not self])
+
+        self.v1.x = (self.v1.x - self.x) / center_pull_coeff
+        self.v1.y = (self.v1.y - self.y) / center_pull_coeff
+
+    # 離脱
+    def separation(self):
+        personal_space = 30
+        for bird in self.neighbors:
+            if bird == self:
+                continue
+            dist = self.calc_distance(bird)
+            if dist == 0:
+                continue
+            if dist < personal_space:
+                self.v2.x -= (bird.x - self.x)
+                self.v2.y -= (bird.y - self.y)
+
+    # 距離の計算
+    def calc_distance(self, target):
+        return math.sqrt((target.x - self.x) ** 2 + (target.y - self.y) ** 2)
+
+    # 整列
+    def alignment(self):
+        if not self.neighbors:
+            return
+
+        self.v3.x = mean([agent.vx for agent in self.neighbors if agent is not self])
+        self.v3.y = mean([agent.vy for agent in self.neighbors if agent is not self])
+
+        self.v3.x = (self.v3.x - self.vx) / 2
+        self.v3.y = (self.v3.y - self.vy) / 2
+
+    def _collision_detection(self):
+        if self.x - Bird.RADIAN < 0:
+            self.vx *= -1
+            self.x = Bird.RADIAN
+        if self.x + Bird.RADIAN > Field.WIDTH:
+            self.vx *= -1
+            self.x = Field.WIDTH - Bird.RADIAN
+
+        if self.y - Bird.RADIAN < 0:
+            self.vy *= -1
+            self.y = Bird.RADIAN
+        if self.y + Bird.RADIAN > Field.HEIGHT:
+            self.vy *= -1
+            self.y = Field.HEIGHT - Bird.RADIAN
+
+    def step(self):
+        self.cohesion()
+        self.separation()
+        self.alignment()
+
+    def update(self):
+        dx = self.r1 * self.v1.x + self.r2 * self.v2.x + self.r3 * self.v3.x
+        dy = self.r1 * self.v1.y + self.r2 * self.v2.y + self.r3 * self.v3.y
+        self.vx += dx
+        self.vy += dy
+
+        distance = (self.vx ** 2 + self.vy ** 2) ** 0.5
+
+        if distance > self.SPEED:
+            self.vx = (self.vx / distance) * self.SPEED
+            self.vy = (self.vy / distance) * self.SPEED
+
+        self.x += int(self.vx)
+        self.y += int(self.vy)
+
+        self._collision_detection()
+
+    def clear_movement(self):
+        self.v1.reset()
+        self.v2.reset()
+        self.v3.reset()
+
+    def draw(self, drawer):
+        self.clear_movement()
+        self.step()
+        self.update()
+        drawer.create_oval(self.x - Bird.RADIAN, self.y - Bird.RADIAN, self.x+Bird.RADIAN, self.y+Bird.RADIAN)
+        drawer.create_line(self.x, self.y, self.x + self.vx * 3, self.y + self.vy * 3)
 
 
+def main():
+    Bird.setup()
+
+    root = Tk()
+
+    canvas = Canvas(root, width=Field.WIDTH, height=Field.HEIGHT)
+    canvas.pack()
+
+    for bird in Bird.birds:
+        bird.find_neighbors()
+
+    def animate():
+        canvas.delete("all")
+        for bird in Bird.birds:
+            bird.draw(canvas)
+        root.after(20, animate)
+
+    animate()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
